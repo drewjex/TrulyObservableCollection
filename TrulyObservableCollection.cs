e187f7f1565ea1eaeae84463cf41bc9d5536e2d4
Drew Jex <drewjex28+1@gmail.com>
2017-09-30 12:17:20 -0600

    Added TrulyObservableCollection.cs

---
+++ TrulyObservableCollection.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace SkyWest.Common.WPF
+{
+    public class TrulyObservableCollection<T> : ObservableCollection<T> where T : INotifyPropertyChanged
+    {
+        public Action<object, PropertyChangedEventArgs> Handler;
+
+        public TrulyObservableCollection()
+            : base()
+        {
+            CollectionChanged += new NotifyCollectionChangedEventHandler(TrulyObservableCollection_CollectionChanged);
+        }
+
+        public TrulyObservableCollection(IEnumerable<T> collection)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+
+            CopyFrom(collection);
+
+            CollectionChanged += new NotifyCollectionChangedEventHandler(TrulyObservableCollection_CollectionChanged);
+        }
+
+        public TrulyObservableCollection(IEnumerable<T> collection, Action<object, PropertyChangedEventArgs> handler)
+        {
+            if (collection == null)
+                throw new ArgumentNullException("collection");
+
+            CopyFrom(collection);
+
+            Handler = handler;
+            foreach (T item in collection)
+            {
+                ((INotifyPropertyChanged)item).PropertyChanged += new PropertyChangedEventHandler(Handler);
+                RecursivelyApplyEventHandler(item);
+            }
+
+            CollectionChanged += new NotifyCollectionChangedEventHandler(TrulyObservableCollection_CollectionChanged);
+        }
+
+        public void RecursivelyApplyEventHandler(INotifyPropertyChanged item) 
+        {
+            foreach (PropertyInfo prop in item.GetType().GetProperties())
+            {
+                if (!prop.PropertyType.IsPrimitive && Convert.GetTypeCode(prop.GetValue(item)) == TypeCode.Object)
+                {
+                    (prop.GetValue(item) as INotifyPropertyChanged).PropertyChanged += new PropertyChangedEventHandler(Handler);
+                    RecursivelyApplyEventHandler((INotifyPropertyChanged)prop.GetValue(item));
+                }
+            }
+        }
+
+        private void CopyFrom(IEnumerable<T> collection)
+        {
+            IList<T> items = Items;
+            if (collection != null && items != null)
+            {
+                using (IEnumerator<T> enumerator = collection.GetEnumerator())
+                {
+                    while (enumerator.MoveNext())
+                    {
+                        items.Add(enumerator.Current);
+                    }
+                }
+            }
+        }
+
+        void TrulyObservableCollection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+            {
+                foreach (Object item in e.NewItems)
+                {
+                    ((INotifyPropertyChanged)item).PropertyChanged += new PropertyChangedEventHandler(Handler);
+                    RecursivelyApplyEventHandler((INotifyPropertyChanged)item);
+                }
+            }
+            if (e.OldItems != null)
+            {
+                foreach (Object item in e.OldItems)
+                {
+                    ((INotifyPropertyChanged)item).PropertyChanged += new PropertyChangedEventHandler(Handler);
+                    RecursivelyApplyEventHandler((INotifyPropertyChanged)item);
+                }
+            }
+        }
+    }
+}
